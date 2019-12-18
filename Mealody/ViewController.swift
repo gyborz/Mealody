018366c3154316d1de2e3bb3aabe1b9194f2b1c0
gyborz <borzgy95@gmail.com>
2019-12-18 17:30:59 +0100

    Initial Commit

---
+++ Mealody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mealody
+//
+//  Created by Gyorgy Borz on 2019. 12. 18..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

8b762681ccdb6b8ab9576f9e9a8df245dffb630a
gyborz <borzgy95@gmail.com>
2019-12-21 23:06:26 +0100

    Add random recipe button, set up it's design

--- Mealody/RandomViewController.swift
+++ Mealody/RandomViewController.swift
@@ -9,11 +9,39 @@
 import UIKit
 
 class RandomViewController: UIViewController {
+    
+    @IBOutlet weak var randomButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.interactivePopGestureRecognizer?.delegate = self
+        
+        setUpButton()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        randomButton.layer.cornerRadius = randomButton.frame.height / 2
+        randomButton.layer.masksToBounds = false
+        randomButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        randomButton.layer.shadowRadius = 10
+        randomButton.layer.shadowColor = UIColor.black.cgColor
+        randomButton.layer.shadowOpacity = 0.5
+    }
+    
+    private func setUpButton() {
+        randomButton.titleLabel?.lineBreakMode = .byWordWrapping
+        let buttonText = "Give me a\nrandom\nrecipe"
+        let coloredText = "random"
+        
+        let range = (buttonText as NSString).range(of: coloredText)
+        
+        let attributedString = NSMutableAttributedString(string: buttonText)
+        attributedString.addAttribute(NSMutableAttributedString.Key.foregroundColor, value: UIColor.systemBackground, range: range)
+
+        randomButton.setAttributedTitle(attributedString, for: .normal)
+        randomButton.titleLabel?.textAlignment = .center
     }
     
     @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {

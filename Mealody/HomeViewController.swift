74df3225ce520ba8fc4e1f78ca68f6a730d18731
gyborz <borzgy95@gmail.com>
2019-12-21 12:14:03 +0100

    Add RandomViewController

--- Mealody/HomeViewController.swift
+++ Mealody/HomeViewController.swift
@@ -17,6 +17,10 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
     }
     
     override func viewWillLayoutSubviews() {
---
+++ Mealody/RandomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RandomViewController.swift
+//  Mealody
+//
+//  Created by Gyorgy Borz on 2019. 12. 21..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class RandomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}

14a920861a0ee0cb627e9f3bbc6610811bfdb026
gyborz <borzgy95@gmail.com>
2019-12-27 13:59:57 +0100

    Add RestManager.swift

---
+++ Mealody/RestManager.swift
@@ -0,0 +1,15 @@
+//
+//  RestManager.swift
+//  Mealody
+//
+//  Created by Gyorgy Borz on 2019. 12. 27..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+
+import Foundation
+
+class RestManager {
+    
+    private let apiKey = "9973533"
+    
+}

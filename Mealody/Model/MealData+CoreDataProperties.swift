bc5c41080e997a45794725f8d57a48f228c2a910
gyborz <borzgy95@gmail.com>
2019-12-27 14:43:27 +0100

    Add MealData core data model

---
+++ Mealody/Model/MealData+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  MealData+CoreDataProperties.swift
+//  Mealody
+//
+//  Created by Gyorgy Borz on 2019. 12. 27..
+//  Copyright © 2019. Gyorgy Borz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension MealData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MealData> {
+        return NSFetchRequest<MealData>(entityName: "MealData")
+    }
+
+    @NSManaged public var idMeal: String?
+    @NSManaged public var mealImage: Data?
+    @NSManaged public var strIngredients: [String]?
+    @NSManaged public var strInstructions: String?
+    @NSManaged public var strMeal: String?
+    @NSManaged public var strMeasures: [String]?
+
+}

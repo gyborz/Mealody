0f818d8f881897fbc4cb1086edde17e33c134ff9
gyborz <borzgy95@gmail.com>
2020-01-14 16:30:59 +0100

    Add CardViewController, set up base UI

---
+++ Mealody/IngredientsVC/CardVC/CardViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CardViewController.swift
+//  Mealody
+//
+//  Created by Gyorgy Borz on 2020. 01. 14..
+//  Copyright © 2020. Gyorgy Borz. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+    @IBOutlet weak var handle: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
